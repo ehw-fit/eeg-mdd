85a46d9c43c51747e0a1e64ba9da83536844cf0f
Vojta Mrazek <mrazek@fit.vutbr.cz>
2023-09-20 09:39:50 +0200

    parameters

---
+++ parameters.py
@@ -0,0 +1,14 @@
+
+classifier_titles = {
+    "dt": "Decision Tree",
+    "ridge": "Ridge",
+    "svm": "SVM",
+    "kneighbors": "k-NN"
+}
+
+classifier_colors = {
+    "dt": "tab:orange",
+    "ridge": "tab:green",
+    "svm": "tab:blue",
+    "kneighbors": "tab:red"
+}
